77e58c07c063d34cb135f52afc1194aa45dce2b5
Глеб Останин <gleb.ostanin2014@yandex.ru>
2023-11-14 14:28:18 +0600

    Added post-request

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from datetime import date
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -24,13 +28,20 @@ users_db = {
 }
 
 
+class User(BaseModel):
+    username: str = Field(min_length=3, max_length=30)
+    date_joined: date
+    location: Optional[str] = None
+    age: int = Field(None, gt=5, lt=130)
+
+
 @app.get("/")
 def first_fastapi():
     return {"Output": "My first FastAPI"}
 
 
 @app.get("/users")
-def get_users_query(limit: int = 2):
+def get_users_query(limit: int = 20):
     user_list = list(users_db.values())
     return user_list[:limit]
 
@@ -38,3 +49,10 @@ def get_users_query(limit: int = 2):
 @app.get("/users/{username}")
 def get_user(username: str):
     return users_db[username]
+
+
+@app.post("/users")
+def create_user(user: User):
+    username = user.username
+    users_db[username] = user.model_dump()
+    return {"message": f"Successfully created user: {username}"}
